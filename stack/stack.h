d3414b7d1bbcb918f9c1e9531bc10b2b31f6ce8a
linzehao <whulinzehao@gmail.com>
2016-10-28 00:15:30 +0800

    Work: stack

--- stack/stack.h
+++ stack/stack.h
@@ -11,7 +11,8 @@ private:
 	Type element;
 	Node<Type>* pointer_to_next;
 	Node<Type>* pointer_to_prior;
-	Stack(Type e=0, Node<Type>* pl=NULL, Node<Type>* pr=NULL): element(e), pointer_to_prior(pl), pointer_to_next(pr) {}
+	Stack(Type e=0, Node<Type>* pl=NULL, Node<Type>* pr=NULL): 
+	    element(e), pointer_to_prior(pl), pointer_to_next(pr) {}
 }
 
 template <typename Type>
@@ -21,9 +22,61 @@ private:
 	Node<Type>* stack_p;
 	Node<Type>* top;
 public:
-	Stack() {}
+	Stack();
+	~Stack();
 	Stack(const Stack&);
 	Stack<Type>& operator=(const Stack&);
+	bool push(const Type&);
+	Type pop();
+	bool clear();
+	bool is_empty() const;
+}
+
+template <typename Type>
+Stack<Type>::Stack()
+{
+	stack_p = top = new Node<Type>();
+}
+
+template <typename Type>
+Stack<Type>::~Stack()
+{
+	clear();
+	delete stack_p;
+}
+
+template <typename Type>
+Stack<Type>::Stack(const Stack& s)
+{
+	stack_p = top = new Node<Type>();
+	Node<Type>* p_original = s.stack_p;
+	while(p_original != s.top)
+	{
+		top -> pointer_to_next = new Node<Type>(*(p_original -> pointer_to_next));
+		top -> pointer_to_next -> pointer_to_prior = top;
+		top = top -> pointer_to_next;
+		p_original = p_original -> pointer_to_next;
+	}
+}
+
+template <typename Type>
+Stack<Type>& Stack<Type>::operator=(const Stack& s)
+{
+	clear();
+	Node<Type>* p_original = s.stack_p;
+	while(p_original != s.top)
+	{
+		top -> pointer_to_next = new Node<Type>(*(p_original -> pointer_to_next));
+		top -> pointer_to_next -> pointer_to_prior = top;
+		top = top -> pointer_to_next;
+		p_original = p_original -> pointer_to_next;
+	}	
+}
+
+template <typename Type>
+bool Stack<Type>::push(const Type& x)
+{
+	
 }
 
 
